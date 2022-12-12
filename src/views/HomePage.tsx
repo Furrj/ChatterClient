ec3f0726c01826d4fcdeff9c25dbea63b957561d
FraterSKS <jackson.a.furr@gmail.com>
2022-12-11 20:04:39 -0500

    homepage

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -4,7 +4,7 @@ import styles from "./HomePage.module.css";
 const HomePage: React.FC = () => {
   return (
     <div className={styles.cont}>
-      <div className={styles.a}>a</div>
+      <div className={styles.a}>b</div>
       <div className={styles.a}>a</div>
     </div>
   );
